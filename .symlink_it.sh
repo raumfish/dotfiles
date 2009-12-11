50c266ccd6c95ecd45cf141c9ec81152a8b42aa9
Shawn Skriver <shawn.skriver@gmail.com>
2009-12-10 19:13:31 -0600

    explicit hostname for linking

--- .symlink_it.sh
+++ .symlink_it.sh
@@ -8,23 +8,23 @@ function relink() {
 
 cd ..
 
-relink .inputrc      ~/etc/inputrc
-relink .bash         ~/etc/bash
-relink .bashrc       ~/etc/bashrc
-relink .bash_profile ~/etc/bash_profile
-relink .bash_logout  ~/etc/bash_logout
+relink ~/.inputrc      ~/etc/inputrc
+relink ~/.bash         ~/etc/bash
+relink ~/.bashrc       ~/etc/bashrc
+relink ~/.bash_profile ~/etc/bash_profile
+relink ~/.bash_logout  ~/etc/bash_logout
 
-relink .vim          ~/etc/vim
-relink .vimrc        ~/etc/vimrc
+relink ~/.vim          ~/etc/vim
+relink ~/.vimrc        ~/etc/vimrc
 
-relink .irbrc        ~/etc/irbrc
-relink .rdebugrc     ~/etc/rdebugrc
+relink ~/.irbrc        ~/etc/irbrc
+relink ~/.rdebugrc     ~/etc/rdebugrc
 
-relink .screenrc     ~/etc/screenrc
+relink ~/.screenrc     ~/etc/screenrc
 
-relink .ssh/config   ~/etc/ssh/config
+relink ~/.ssh/config   ~/etc/ssh/config
 
-relink .gitconfig    ~/etc/gitconfig
+relink ~/.gitconfig    ~/etc/gitconfig
 
-relink .rtorrent.rc  ~/etc/rtorrent.rc
+relink ~/.rtorrent.rc  ~/etc/rtorrent.rc
 
