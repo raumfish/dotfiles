1a194a521f31e3e4810712e891c5cdaa3ed0dadf
Shawn Skriver <Shawn.Skriver@gmail.com>
2010-06-24 09:38:26 -0500

    add parallelrc; update symlink create

--- .symlink_it.sh
+++ .symlink_it.sh
@@ -9,35 +9,24 @@ function relink() {
 cd ..
 
 relink ~/.inputrc      ~/etc/inputrc
-
 relink ~/.bash         ~/etc/bash
 relink ~/.bashrc       ~/etc/bashrc
 relink ~/.bash_profile ~/etc/bash_profile
 relink ~/.bash_logout  ~/etc/bash_logout
-
 relink ~/.vim          ~/etc/vim
 relink ~/.vimrc        ~/etc/vimrc
 relink ~/.gvimrc       ~/etc/gvimrc
-
 relink ~/.irbrc        ~/etc/irbrc
 relink ~/.rdebugrc     ~/etc/rdebugrc
-
 relink ~/.screenrc     ~/etc/screenrc
-
 relink ~/.gitconfig    ~/etc/gitconfig
 relink ~/.gitignore    ~/etc/gitignore
-
 relink ~/.ssh/config   ~/etc/ssh/config
-
 relink ~/.rtorrent.rc  ~/etc/rtorrent.rc
-
 relink ~/.wgetrc       ~/etc/wgetrc
-
 relink ~/.gemrc        ~/etc/gemrc
-
 relink ~/.emacs        ~/etc/emacs
-
-relink ~/.zile        ~/etc/zile
-
-relink ~/.curlrc      ~/etc/curlrc
-
+relink ~/.zile         ~/etc/zile
+relink ~/.curlrc       ~/etc/curlrc
+relink ~/.pssh         ~/etc/pssh
+relink ~/.parallelrc   ~/etc/parallelrc
